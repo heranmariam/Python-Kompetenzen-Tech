a4f1cfa6dba72922839601199782209871fd9676
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2025-02-05 12:29:51 +0530

    Add files via upload

---
+++ review.py
@@ -0,0 +1,75 @@
+# number = [1, 2, 3, 4, 5]
+# for n in number:
+#     print(f"The square of {n} is {n ** 2}")
+
+
+# data=[20,30,40,50]
+# n=[]
+# for i in data:
+#     n.append(i*i)
+# print(n)
+
+
+
+# problem: Find the largest number in a list. 
+
+# n1 = int(input("enter the number 1: "))
+# n2 = int(input("enter the number 2: "))
+# n3 = int(input("enter the number 3: "))
+# if n1 > n2  and n1 > n3:
+#     print(f"{n1} is greater")
+    
+# elif n2 > n1 and n2 > n3:
+#      print(f"{n2} is greater")
+     
+# else:
+#      print(f"{n3} is greater")
+
+
+##################### Problem: Remove duplicates from a list.  ##################################
+
+# # Example list with duplicates
+# my_list = [1, 2, 2, 3, 4, 4, 5]
+
+# # Removing duplicates by converting to a set and back to a list
+# unique_list = list(set(my_list))
+
+# print(unique_list)
+
+
+# or
+
+
+# a=[1,2,3,3,4,5,6,6,6]
+# print(set(a))
+
+################### Problem: Reverse a list without using slicing  ???????################################
+
+
+# a = [1,2,3,3,4,5,6]
+# a.reverse()
+# print(a)
+
+# Example list
+# my_list = [1, 2, 3, 4, 5]
+
+# # Reversing the list using a simple for loop
+# reversed_list = []
+# for i in my_list:
+#     reversed_list = [i] + reversed_list
+
+# print(reversed_list)
+
+
+
+
+
+    
+    
+     
+    
+    
+
+
+
+
