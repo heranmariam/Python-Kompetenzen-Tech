2c79c55dffe11249fad0c3a243681e1a6799cae5
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2025-02-05 12:28:50 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,38 @@
+# # # row=int(input("enter num of rows"))
+# # # for i in range(row):
+# # #     i=i+1
+# # #     for j in range(i):
+# # #         j=j+1
+# # #         print(i ,end="")
+# # #     print("\r")
+    
+    
+    
+# # # row = int(input("Enter number of rows: "))
+# # # for i in range(row):
+# # #     for j in range(i + 1):  # No need to increment `j` explicitly
+# # #         print("*", end="")
+# # #     print()  # Move to the next line after each row
+
+
+# # # row = int(input("Enter number of rows: "))
+# # # for i in range(row, 0, -1):  # Start from 'row' and decrement to 1, 0 koduthath means ethra step vech aanu reverse aakendath allaki if 0ku pakaram 1 koduthal oru step kazhijee aduthath eru oru step skip aavum  ############
+# # #     for j in range(i):       # Print 'i' asterisks in each row
+# # #         print("*", end="")
+# # #     print()  # Move to the next line after each row
+
+
+
+# # row = int(input("Enter number of rows: "))
+# # for i in range(1, row + 1):  # Start from 1 to 'row'
+# #     # Print increasing part
+# #     for j in range(1, i + 1):
+# #         print(j, end="")
+# #     # Print decreasing part
+# #     for j in range(i - 1, 0, -1):
+# #         print(j, end="")
+# #     print()  # Move to the next line after each row
+
+
+
+
